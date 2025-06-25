76995130d17cee00249b1e315c1a825a7527f4c2
Marquan Bowman <55892092+MarquanBow@users.noreply.github.com>
2025-06-25 17:38:45 -0400

    update firebaseConfig to import getAuth

--- expense-tracker/frontend/src/firebaseConfig.js
+++ expense-tracker/frontend/src/firebaseConfig.js
@@ -1,11 +1,7 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
 import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyC14LKCDrTulwNK_YoMUhgdRCnzg2x4r90",
   authDomain: "expense-tracker-f698b.firebaseapp.com",
@@ -16,7 +12,6 @@ const firebaseConfig = {
   measurementId: "G-SN4DSSVMLF"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 const analytics = getAnalytics(app);
