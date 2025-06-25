36575a37bd46119f63fd1ea0301d9a72f65d23f7
Marquan Bowman <55892092+MarquanBow@users.noreply.github.com>
2025-06-25 16:12:37 -0400

    remove unneeded files

--- expense-tracker/backend/database.py
+++
@@ -1,7 +0,0 @@
-from motor.motor_asyncio import AsyncIOMotorClient
-import os
-
-MONGO_URI = os.environ.get("MONGO_URI", "your-local-or-default-connection")
-
-client = AsyncIOMotorClient(MONGO_URI)
-db = client.expenses_db
